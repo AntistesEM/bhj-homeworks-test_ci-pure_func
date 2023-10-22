da49c6a288d206073a1ce3db286ff7701326caad
Mashkin E.V <mev.86@list.ru>
2023-10-22 14:51:40 +0300

    Initial commit

---
+++ src/js/__tests__/test_pure_functions.js
@@ -0,0 +1,10 @@
+import pureFunctions from '../pure_functions';
+
+test.each([
+  [92, 'healthy'],
+  [40, 'wounded'],
+  [10, 'critical'],
+])('testing pure function with %i health', (health, expected) => { 
+  const result = pureFunctions(health);
+  expect(result).toBe(expected);
+});
---
+++ src/js/pure_functions.js
@@ -0,0 +1,11 @@
+export default function pureFunctions(value) {
+	if (value >= 50) {
+		return "healthy";
+	}
+	if (value >= 15 & value < 50) {
+		return "wounded";
+	}
+	if (value < 15) {
+		return "critical";
+	}
+}
